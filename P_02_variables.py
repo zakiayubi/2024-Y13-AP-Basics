7ede012f3124f0461e65f860d380efaac05808e5
zakiayubi <132313595+zakiayubi@users.noreply.github.com>
2024-03-27 10:06:53 +1300

    Create P_02_variables.py

---
+++ P_02_variables.py
@@ -0,0 +1,16 @@
+# Codings to learn about the variables..
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variable can be a number
+fav_num = 7
+num_string = "5"
+
+print(fav_num * 2)
+print(num_string * 5)
+
+# Constants are uppercase
+PI = 3.14
+# Starting should start with lower case letters and not num or upper case 
+a_01_my_num = 7
