2847b0eb3d20d1f1ec8dd2e83b5c86353dd7f01b
zakiayubi <132313595+zakiayubi@users.noreply.github.com>
2024-03-27 10:09:19 +1300

    Create P_03_input_output.py

---
+++ P_03_input_output.py
@@ -0,0 +1,33 @@
+ # ask for their name
+username = input("What is your name? ")
+
+# ask the user for their favourite number (integer)
+fav_num = int(input("What is your favourite number? "))
+
+
+
+# Double, halve and square the user's favourite number
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+
+# Greet user
+
+print()
+print(f"Hi {username}, your favourite number number is {fav_num}.")
+print()
+ 
+# Output the results of doubling, halving and
+# sqauring their favourite integer
+
+
+
+print(f"Double {fav_num} is {double}.")
+print(f"Half {fav_num} is {halve}.")
+print(f"{fav_num} sqaured is {square}.")
+
+# Below is how you leave a space
+print()
+
+print("Have an awesome day.")
